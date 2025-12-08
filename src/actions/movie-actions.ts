be2bb718b6c163dfd87b3f2fd39be4b5540abb4c
Keeso1 <isac.lindh01@gmail.com>
2025-12-08 13:28:20 +0100

    added genre to moviecard

--- src/actions/movie-actions.ts
+++ src/actions/movie-actions.ts
@@ -3,15 +3,25 @@
 import prisma from "@/lib/prisma"
 
 export const getMovies = async () => {
-    return await prisma.movie.findMany();
+    return await prisma.movie.findMany({
+        include: {
+            genres: true,
+            moviePersons: true
+        }
+    });
 }
 
 export const getMovieById = async (id: string) => {
     return await prisma.movie.findUnique({
         where: {
             id
+        },
+        include: {
+            genres: true,
+            moviePersons: true
         }
     });
 }
 
-export type Movies = Awaited<ReturnType<typeof getMovies>>;
+export type getMoviesType = Awaited<ReturnType<typeof getMovies>>;
+export type getMovieType = getMoviesType[0]
--- src/components/movieCardContainer.tsx
+++ src/components/movieCardContainer.tsx
@@ -1,11 +1,8 @@
-import { MovieModel } from "../../generated/prisma/models";
 import MovieCard from "./movieCard";
 
-interface MovieCardContainerProps {
-  movies: MovieModel[];
-}
+import { getMoviesType } from "@/actions/movie-actions";
 
-export default function MovieCardContainer({ movies }: MovieCardContainerProps) {
+export default function MovieCardContainer({ movies }: { movies: getMoviesType }) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
       {movies.map((movie) => (
