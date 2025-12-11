196801cc0cbea7453afbf605b55189adc63cc8e1
Keeso1 <isac.lindh01@gmail.com>
2025-12-11 12:52:52 +0100

    genrename fix and stockprice added

--- src/components/movieCard.tsx
+++ src/components/movieCard.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import { getMovieType } from "@/actions/movie-actions";
+import { authClient } from "@/lib/auth-client";
 
 export default function MovieCard({ movie }: { movie: getMovieType }) {
+  
   return (
     <Link
-      href={`/movies/${movie.id}`}
+      href={authClient.admin ? `/admin/movies/${movie.id}` : `/movies/${movie.id}`}
       className="flex flex-col flex-wrap bg-white rounded-lg shadow hover:shadow-lg transition-shadow overflow-hidden"
     >
       <div className="relative bg-gray-200">
@@ -34,17 +36,15 @@ export default function MovieCard({ movie }: { movie: getMovieType }) {
           {movie.runtime !== null && <span>{movie.runtime} min</span>}
         </div>
 
-        <div className="mt-2 flex flex-wrap gap-3 text-sm text-gray-600">
-          <span>
-            {movie.genres.map((genre) => (
-              <p key={genre.id}>{genre.name}</p>
-            ))}
-          </span>
+        <div className="mt-2 text-sm text-gray-600 truncate">
+          <p>{movie.genres.map(genre => genre.name).join(' • ')}</p>
         </div>
 
         {movie.price !== null && (
-          <div className="mt-4 text-green-600 font-semibold">
-            ${movie.price.toFixed(2)}
+          <div className="flex flex-row justify-between mt-4 text-green-600 font-semibold">
+            <p>${movie.price.toFixed(2)}</p>
+            <p>{movie.stock} in stock</p>
+            
           </div>
         )}
       </div>
