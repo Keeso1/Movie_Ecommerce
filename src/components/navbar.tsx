cfa9a68ab76813c9e8b49170126dbc5e2bc42d93
Keeso1 <isac.lindh01@gmail.com>
2026-01-05 12:54:04 +0100

    updated icon

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,11 @@
-'use client';
+"use client";
 
 import Link from "next/link";
 import { useCart } from "@/context/CartContext";
-import { authClient } from "@/lib/auth-client"
+import { authClient } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
 import { Button } from "./ui/button";
-
+import { ShoppingCartIcon } from "lucide-react";
 export default function Navbar() {
   const session = authClient.useSession();
   const router = useRouter();
@@ -16,33 +16,38 @@ export default function Navbar() {
       <Link href="/" className="font-bold text-lg">
         Movie Store
       </Link>
+
       <div className="flex gap-1">
         <Link href="/cart" className="relative">
-            🛒
-            {getTotalItems() > 0 && (
-              <span className="absolute -top-2 -right-3 bg-red-600 text-white text-xs rounded-full px-2">
-                {getTotalItems()}
-              </span>
-            )}
-          </Link>
-          <p>{session.data?.user.name}</p>
-          {session.data === null ? (
-              <>
-              <Button asChild>
-                  <Link href={"/signup"}>SignUp</Link>
-              </Button>
-              <Button asChild>
-                  <Link href={"/signin"}>SignIn</Link>
-              </Button>
-              </>) : 
-              (<>
-              <Button onClick={async () =>{
-                  await authClient.signOut();
-                  router.refresh();
-              }}>
-                  Sign Out
-              </Button>
-            </>)}
+          <ShoppingCartIcon></ShoppingCartIcon>
+          {getTotalItems() > 0 && (
+            <span className="absolute -top-2 -right-3 bg-red-600 text-white text-xs rounded-full px-2">
+              {getTotalItems()}
+            </span>
+          )}
+        </Link>
+        <p>{session.data?.user.name}</p>
+        {session.data === null ? (
+          <>
+            <Button asChild>
+              <Link href={"/signup"}>SignUp</Link>
+            </Button>
+            <Button asChild>
+              <Link href={"/signin"}>SignIn</Link>
+            </Button>
+          </>
+        ) : (
+          <>
+            <Button
+              onClick={async () => {
+                await authClient.signOut();
+                router.refresh();
+              }}
+            >
+              Sign Out
+            </Button>
+          </>
+        )}
       </div>
     </nav>
   );
