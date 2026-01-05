5718292da504c0c3f8026315add521ffc0f0ebeb
Manoj-Axelsson <mr.mjaxelsson@gmail.com>
2026-01-05 11:35:08 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,20 +3,21 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import { CartProvider } from "@/context/CartContext";
+import ThemeProvider from "@/components/ThemeProvider";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Movie Store",
-  description: "Movie shopping app",
+	title: "Movie Store",
+	description: "Movie shopping app",
 };
 
 export default function RootLayout({
@@ -25,8 +26,8 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en" suppressHydrationWarning={true}>
-			<body className="h-full antialiased">
+		<html lang="en">
+			<body className={`${geistSans.variable} ${geistMono.variable} antialiased h-full flex flex-col`}>
 				<ThemeProvider>
 					<CartProvider>
 						<Navbar />
